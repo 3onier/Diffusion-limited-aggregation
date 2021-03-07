5f4bd88d45b094390beb59b770fb7f4aa2fabccb
3onier <30025379+3onier@users.noreply.github.com>
2021-03-07 13:53:20 +0100

    Adapted renderer colouring init particle

--- renderer.py
+++ renderer.py
@@ -4,13 +4,18 @@ from matplotlib import pyplot as plt
 height, width = 0, 0  # int(sys.stdin[0]), int(sys.stdin[1])
 list_x = []
 list_y = []
+init_x, init_y = 0, 0
 
 inp = ""
 
-for s in sys.stdin:
-    x, y = s.split()
-    list_x.append(int(x))
-    list_y.append(int(y))
+for i, s in enumerate(sys.stdin):
+    x, y = map(int, s.split())
+    if i == 0:
+        init_x, init_y = x, y
+        continue
+    list_x.append(x)
+    list_y.append(y)
 
-plt.scatter(list_x, list_y)
+plt.scatter(list_x, list_y, color="blue", marker=".")
+plt.scatter(init_x, init_y, color="red")
 plt.show()
